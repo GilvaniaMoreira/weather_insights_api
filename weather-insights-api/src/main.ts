b5f194b7c1a3a1d1d531d02961c00481eb61db6b
GilvaniaMoreira <moreiragil84@gmail.com>
2025-11-08 06:28:51 -0300

    chore: Saffold nest applicantion sctructure

---
+++ weather-insights-api/src/main.ts
@@ -0,0 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter(httpAdapterHost));
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Weather Insights API')
+    .setDescription('API para consultar dados climáticos e gerar insights de temperatura')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
